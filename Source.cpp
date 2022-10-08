6e5597db24f42aeebc6110812dc1729a4e29d0d9
ilyaLobeuko <114007539+ilyaLobeuko@users.noreply.github.com>
2022-10-08 09:30:01 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	// 1.
+	/*int a, b, c;
+	cin >> a;
+	if (a == 0 || a == 2)
+	{
+		b = a + 7;
+		cout << b << endl;
+	} 
+	else 
+	{
+		c = a - 10;
+		cout << c << endl;
+	}*/
+	
+	// 2.
+	/*int a, b, c, d;
+	cin >> a >> b;
+	if (a <= 1 && b >= 3) {
+		c = a + b;
+		cout << c << endl;
+	}
+	else {
+		d = a - b;
+		cout << d << endl;
+	}*/
+	
+	// 3.
+	/*int a, b;
+	cin >> a >> b;
+	if (a > 2 && a < 11 || b >= 6 && b < 14) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}*/
+	
+	// 4.
+	/*int day;
+	cin >> day;
+	switch (day) {
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10: cout << "1" << endl; break;
+	case 11:
+	case 12:
+	case 13:
+	case 14:
+	case 15:
+	case 16:
+	case 17:
+	case 18:
+	case 19:
+	case 20: cout << "2" << endl; break;
+	case 21:
+	case 22:
+	case 23:
+	case 24:
+	case 25:
+	case 26:
+	case 27:
+	case 28:
+	case 29:
+	case 30:
+	case 31: cout << "3" << endl; break;
+	}*/
+	
+	// 5.
+	/*int month;
+	cin >> month;
+	switch (month) {
+	case 1:
+	case 2:
+	case 12: cout << "WINTER" << endl; break;
+	case 3:
+	case 4:
+	case 5: cout << "SPRING" << endl; break;
+	case 6:
+	case 7:
+	case 8: cout << "SUMMER" << endl; break;
+	case 9:
+	case 10:
+	case 11: cout << "AUTUMN" << endl; break;
+	}*/
+}
